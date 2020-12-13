2db4707a73dad626aeaa725e3f32ea031a041ed9
Nghia Nguyen <naguyen22@gmail.com>
2020-12-13 08:35:12 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Import packages
+const express = require(`express`);
+const bodyParser = require(`body-parser`)
+const cors = require(`cors`)
+
+// create instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+function listening() {
+  console.log(`server running on port: ${port}`);
+}
+
+
