9a6c323e469df042908588fffd56cf302022e975
Nghia Nguyen <naguyen22@gmail.com>
2020-12-13 18:32:59 -0500

    build API url

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ projectData = {};
 
 // Import packages
 const express = require(`express`);
-const bodyParser = require(`body-parser`)
-const cors = require(`cors`)
+const bodyParser = require(`body-parser`);
+const cors = require(`cors`);
 
 // create instance of app
 const app = express();
 
-/* Middleware*/
+/* Middleware */
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -18,7 +19,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Initialize the main project folder (connect server side to client side code)
-app.use(express.static('demo'));
+app.use(express.static("website"));
 
 // Setup Server
 const port = 8000;
@@ -28,16 +29,19 @@ function listening() {
   console.log(`server running on port: ${port}`);
 }
 
-
-// ROUTING
+/* ROUTING */
 
 // handles GET requests from client
-app.get(`/`,(req, res) => {
-  // console.log(req);
-  res.send(projectData)
-})
+app.get(`/`, (req, res) => {
+  res.send(projectData);
+});
 
 // handles POST requests from client
-app.post(`/`,(req, res) => {
-  projectData.push(req.body)
-})
+app.post(`/addName`, addData);
+
+// add incoming data to our endpoint object
+function addData(req, res) {
+  const incomingData = req.body;
+  projectData.name = incomingData.name;
+  console.log(projectData);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,38 @@
 /* Global Variables */
 
+// build API URL
+const baseURL = `api.openweathermap.org/data/2.5/weather?zip=`;
+const apiKey = `01230402996a2c0bcff6e0ba4ce12d17`;
+const zipCode = document.querySelector(`#zip`).value;
+
+const apiCall = `${baseURL}${zipCode}&${apiKey}`;
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+// Post data function
+const postData = async (url = "", data = {}) => {
+
+  console.log(data);
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // Body data type must match "Content-Type" header
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+postData("/addName", { name: "Nghia" });
+postData("/addName", { name: "Ron" });
