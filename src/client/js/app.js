7e5ef5f7e0b496fa2368d21a6122a4347207b9b8
Nghia Nguyen <naguyen22@gmail.com>
2020-12-25 17:33:02 -0500

    add getDateDiff

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -3,72 +3,93 @@ const generateBtn = document.querySelector(`#generate`);
 
 /* Helper Functions */
 function getCurrentDate() {
-    const d = new Date();
-    return d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+	const d = new Date();
+	return (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
+}
+
+function getDateDiff(tripDate) {
+	const todaysDate = new Date(getCurrentDate());
+	const tripDate = new Date(tripDate);
+	const diffTime = Math.abs(tripDate - todaysDate);
+	const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+	return diffDays;
 }
 
 /* Main Functions */
 // Post data to server
 const postData = async (url = "", data = {}) => {
-    const options = {
-        method: "POST",
-        credentials: "same-origin",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        // Body data type must match "Content-Type" header
-        body: JSON.stringify({ data }),
-    };
+	const options = {
+		method: "POST",
+		credentials: "same-origin",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		// Body data type must match "Content-Type" header
+		body: JSON.stringify({ data }),
+	};
 
-    try {
-        const response = await fetch(url, options);
-        return response.text();
-    } catch (error) {
-        console.log(`error`, error);
-    }
+	try {
+		const response = await fetch(url, options);
+		return response.text();
+	} catch (error) {
+		console.log(`error`, error);
+	}
 };
 
 // Get data from server
 const getData = async (url = "") => {
-    try {
-        const response = await fetch(url);
-        return response.json();
-    } catch (error) {
-        console.log(`error`, error);
-    }
+	try {
+		const response = await fetch(url);
+		return response.json();
+	} catch (error) {
+		console.log(`error`, error);
+	}
 };
 
 async function clickHandler() {
-    const city = document.querySelector(`#city`).value;
-    const date = document.querySelector(`#departure`).value;
-    const userInput = { city, date }
+	const city = document.querySelector(`#city`).value;
+	const departure = document.querySelector(`#departure`).value;
+	const userInput = { city, departure };
 
-    // get weather data
-    const res = await postData('http://localhost:8000/send', userInput);
+	// validate date input format (some form of regex handler)
+    validateInputDate()
 
-    // extract temperature
-    const weatherObject = await JSON.parse(res)
+	// check how far trip date is from today's date
+	const dateDiff = getDateDiff(departure);
 
-    // get current date
-    const td = getCurrentDate();
+	// if results is within a week
+	if (dateDiff <= 7) {
+		// get current forecast
+	} else {
+		// get predicted forecast
+	}
 
-    // get user's feeling
-    const userFeeling = document.querySelector(`#departure`).value;
+	// get weather data
+	const res = await postData("http://localhost:8000/send", userInput);
 
-    // build object to be sent
-    const data = {
-        temperature: `${temp}°F`,
-        date: td,
-        userFeeling: userFeeling,
-    };
+	// extract temperature
+	const weatherObject = await JSON.parse(res);
 
-    // send data to server
-    const post = await postData(`http://localhost:8000/add`, data);
+	// get current date
+	const td = getCurrentDate();
 
-    // check if post was successful
-    if (post === `success!`) {
-        updateEntry();
-    }
+	// get user's feeling
+	const userFeeling = document.querySelector(`#departure`).value;
+
+	// build object to be sent
+	const data = {
+		temperature: `${temp}°F`,
+		date: td,
+		userFeeling: userFeeling,
+	};
+
+	// send data to server
+	const post = await postData(`http://localhost:8000/add`, data);
+
+	// check if post was successful
+	if (post === `success!`) {
+		updateEntry();
+	}
 }
 
 /* Event listener */
@@ -76,18 +97,18 @@ generateBtn.addEventListener(`click`, clickHandler);
 
 // Function to update most recent entry using data from server
 async function updateEntry() {
-    const date = document.getElementById(`date`);
-    const temp = document.getElementById(`temp`);
-    const content = document.getElementById(`content`);
+	const date = document.getElementById(`date`);
+	const temp = document.getElementById(`temp`);
+	const content = document.getElementById(`content`);
 
-    try {
-        const entry = await getData(`http://localhost:8000/retrieve`);
-        date.innerText = entry.date;
-        temp.innerText = entry.temperature;
-        content.innerText = entry.userFeeling;
-    } catch (error) {
-        console.log(`error`, error);
-    }
+	try {
+		const entry = await getData(`http://localhost:8000/retrieve`);
+		date.innerText = entry.date;
+		temp.innerText = entry.temperature;
+		content.innerText = entry.userFeeling;
+	} catch (error) {
+		console.log(`error`, error);
+	}
 }
 
-export { clickHandler, generateBtn }
+export { clickHandler, generateBtn };
